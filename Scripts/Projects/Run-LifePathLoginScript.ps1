f0b647ceaae1104593a22b618d5ffb9621d06b79
Wayne Reeves <wreeves@ccmhmr.local>
2019-08-09 10:44:02 -0500

    8-9-19

---
+++ Scripts/Projects/Run-LifePathLoginScript.ps1
@@ -0,0 +1,21 @@
+﻿Write-Progress -Activity "Running LifePath Logon Script" -Status "Starting" -PercentComplete 0
+Write-Progress -Activity "Running LifePath Logon Script" -Status "Refreshing DNS" -PercentComplete 5
+ipconfig /flushdns
+ipconfig /registerdns
+Write-Progress -Activity "Running LifePath Logon Script" -Status "Testing Connectivity" -PercentComplete 10
+if ( Test-Connection DC01.ccmhmr.local -Count 1 -BufferSize 1 -Quiet )
+	{
+	$server = "DC01.ccmhmr.local"
+	}
+else
+	{
+	$server = "DC02.ccmhmr.local"
+	}
+Write-Progress -Activity "Running LifePath Logon Script" -Status "Getting Login Script" -PercentComplete 50
+$domain = [Environment]::UserDomainName
+$username = [Environment]::UserName
+$User = [ADSI]("WinNT://" + $server + "/" + $username)
+$script = "\\" +$domain + "\netlogon\" + $User.LoginScript
+Write-Progress -Activity "Running LifePath Logon Script" -Status "Running Login Script" -PercentComplete 95
+Start-Process $script
+Write-Progress -Activity "Running LifePath Logon Script" -Status "Complete" -PercentComplete 100
